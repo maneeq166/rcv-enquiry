8d852aa5e825600ddf5379c012ce919871cc7e2e
Nitin6404 <nc1575612@gmail.com>
2025-02-10 15:02:26 +0530

    fix: add error handling OAUTH Client

--- utils/oAuthClient.js
+++ utils/oAuthClient.js
@@ -1,10 +1,14 @@
 const { google } = require("googleapis");
+require("dotenv").config();
 
-// OAuth2 Credentials for Gmail API
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.REDIRECT_URI) {
+    throw new Error("Missing required OAuth2 environment variables");
+}
+
 const oAuth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
     process.env.REDIRECT_URI
-  );
+);
 
 module.exports = oAuth2Client;
